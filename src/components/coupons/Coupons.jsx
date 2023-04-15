22650a2896291be2a014419f3cd015f57c4fe5f2
manan2911 <mananbhatt2911@gmail.com>
2023-04-15 20:31:57 +0530

    fourth

--- src/components/coupons/Coupons.jsx
+++ src/components/coupons/Coupons.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Coupons.css";
 
 const Coupons = () => {
+  const [couponCode, setCouponCode] = useState("");
+  const [status, setStatus] = useState("Not Applied");
+
+  const handleCouponCodeChange = (event) => {
+    setCouponCode(event.target.value);
+  };
+
+  const applyCoupon = () => {
+    // Check if the coupon code is valid and set the status accordingly
+    if (couponCode === "VALID_COUPON_CODE") {
+      setStatus("Applied");
+    } else {
+      setStatus("Invalid");
+    }
+  };
+
   return (
     <div className="containerCoupon">
       <div className="upper">
@@ -10,7 +26,15 @@ const Coupons = () => {
         <h2>Amount saved</h2>
       </div>
       <div className="lower">
-        <h3>Coupon Applied Succesfully!</h3>
+        <input
+          type="text"
+          placeholder="Enter coupon code"
+          value={couponCode}
+          onChange={handleCouponCodeChange}
+        />
+        <button onClick={applyCoupon}>Apply Coupon</button>
+        {status === "Applied" && <h3>Coupon Applied Successfully!</h3>}
+        {status === "Invalid" && <h3>Invalid Coupon Code!</h3>}
       </div>
     </div>
   );
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "../form/From.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+const Form = () => {
+  return (
+    <div>
+      <div className="main-title-form">
+        <h2>ASK US ANYTHING</h2>
+      </div>
 
-function Form() {
-  return <div>Form</div>;
-}
+      <div className="input-text-area">
+        <textarea placeholder="Ask us anything" />
+      </div>
+
+      <button className="whatsapp-button">
+        <FontAwesomeIcon icon={faWhatsapp} className="whatsapp-icon" />
+        WhatsApp
+      </button>
+      <div className="contact-us">
+        <p>
+          Or just email us at{" "}
+          <a href="mailto:info@seekhoapp.com">info@seekhoapp.com</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
